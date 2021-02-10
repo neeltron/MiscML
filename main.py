cf8df15a6f134357001d63fde00f52bb91d368f1
unknown <500075823@stu.upes.ac.in>
2021-02-10 22:59:38 +0530

    initiate the main screen

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-class MiscML(Widget):
+class MainWindow(Screen):
     pass
 
 
+class SecondWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file("MiscML.kv")
+
+
 class MiscMLApp(App):
-    def build(self):
-        return MiscML()
+    def build(self): 
+        return kv
 
 
-MiscMLApp().run()
+if __name__ == "__main__":
+    MiscMLApp().run()
