dda70ae70939df8697dd7fa59cee3acf0f2e037d
unknown <500075823@stu.upes.ac.in>
2021-02-10 17:52:38 +0530

    raw GUI code

--- main.py
+++ main.py
@@ -2,4 +2,13 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 
 
-PongGame()
+class MiscML(Widget):
+    pass
+
+
+class MiscMLApp(App):
+    def build(self):
+        return MiscML()
+
+
+MiscMLApp().run()
